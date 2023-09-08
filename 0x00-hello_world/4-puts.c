0a5a2756cec447d0f785a226ca8bf54fc6a0ede4
Raphael4050 <khobbykay399@gmail.com>
2023-09-07 17:19:40 -0700

    Hello World C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#iclude<stdio.h>
+/**
+ * main  - Entry point 
+ *
+ * Return: Alway 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle
+");
+	return (0);
+}
+
